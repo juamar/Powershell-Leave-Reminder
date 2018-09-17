8687dc4affeb79c5bab04d621cfa64db2ffc8451
Avendaño Huergo <jiavendano@indra.es>
2018-09-17 17:52:22 +0200

    init commit

---
+++ Leave.ps1
@@ -0,0 +1,23 @@
+﻿$scriptBlock = {
+    Add-Type -AssemblyName System.Windows.Forms
+
+    [System.Windows.Forms.MessageBox]::Show('It `s 6 PM. Complete the Jira and leave now!', 'Time to leave', 'Ok', 'Warning')
+}
+
+Start-Job $scriptBlock | Out-Null
+
+$spaces = 0
+
+while (Get-Job -State "Running")
+{
+    $spaceString = ""
+
+    for ($i=0; $i -lt $spaces; $i++)
+    {
+        $spaceString = $spaceString + " "
+    }
+
+    echo $spaceString'Go home!'
+    $spaces = $spaces + 4
+    Start-Sleep -Milliseconds 100
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+##Powershell Leave Reminder##
+
+Simple, but not less funny, Powershell script which alert us to leave. You can configure this with a windows Scheduled task in order to make it work properly.
+
+Next iterations:
+
+-Instructions on this readme to configure the schedule task.
+-Build an installer to automatically make the scheduled task.
